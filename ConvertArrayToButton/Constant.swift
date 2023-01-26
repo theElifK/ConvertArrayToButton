0d965c3854bdab8ec367dc0f0ffdf377c86ec793
theElifK <e.karakolcu@hotmail.com>
2023-01-26 17:09:10 +0300

    Added Constant File

---
+++ ConvertArrayToButton/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  ConvertArrayToButton
+//
+//  Created by Elif Karakolcu on 26.01.2023.
+//
+
+import Foundation
+class Constant {
+  static let citysArray = ["Adana","Adiyaman","Afyon","Agri","Aksaray","Amasya","Ankara","Antalya","Ardahan","Artvin","Aydin","Balikesir","Bartin","Batman","Bayburt","Bilecik","Bingol","Bitlis","Bolu","Burdur","Bursa","Canakkale","Cankiri","Corum","Denizli","Diyarbakir","Duzce","Edirne","Elazig","Erzincan","Erzurum","Eskisehir","Gaziantep","Giresun","Gumushane","Hakkari","Hatay","Igdir","Isparta","Istanbul","Izmir","Kahramanmaras","Karabuk","Karaman","Kars","Kastamonu","Kayseri","Kilis","Kirikkale","Kirklareli","Kirsehir","Kocaeli","Konya","Kutahya","Malatya","Manisa","Mardin","Mersin","Mugla","Mus","Nevsehir","Nigde","Ordu","Osmaniye","Rize","Sakarya","Samsun","Sanliurfa","Siirt","Sinop","Sirnak","Sivas","Tekirdag","Tokat","Trabzon","Tunceli","Usak","Van","Yalova","Yozgat","Zonguldak"]
+}
